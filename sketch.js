32a94699972f93109216e360a35e85c7e82f7d61
AlyxFreeman <ziyuan070109@gmail.com>
2025-12-09 18:11:20 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -119,7 +119,7 @@ function preload() {
 
   //AUDIO//
   //sun
-  SunFlash = loadSound("assets\SunFlash.mp3")
+  SunFlash = loadSound("assets/SunFlash.mp3")
   sunExpand = loadSound("assets/SunExpand.mp3")
   //background
   cosmicBg = loadSound("assets/CosmicBg.mp3")
